cfa0097a263cae46809f2406af59751c6cf56bcc
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-25 12:55:17 -0400

    Add more comments regarding render method

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,15 @@
+# CONTROLLER
 class ProductsController < ApplicationController
 
+  # ACTION
   def index
     # uses MODEL
     @products = Product.all.order(created_at: :desc)
+
     # VIEW
-    # render :index <- this is implicit in Rails and runs behind the scenes if there's nothing else special going on (ex. redirects)
+    # render :index 
+    # ^ this is implicit in Rails and runs behind the scenes if there's nothing else special going on (ex. redirects)
+    # If you don't render anything at the end of an action, it will render the name of that action as a template
   end
 
   def show
