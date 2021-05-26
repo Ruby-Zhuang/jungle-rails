4054e2665d9dec50198324dc1f81b7d8d3aa4d8a
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-26 19:52:08 -0400

    Update validation to ensure password and re-typed password are the same

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
       redirect_to '/signup'
     end
   end
-  
+
   private
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   # gives User model authentication methods via bcrypt.
   has_secure_password
+
+  # two fields should receive exactly the same content
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
