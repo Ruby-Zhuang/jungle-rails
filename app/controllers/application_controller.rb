17d79a10e089a27f73394dfb95fd78b12efa7895
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-26 19:05:20 -0400

    Update the application controller with new methods to look up the user, if they're logged in, and save their user object to a variable called @current_user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,18 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # look up the user, if they're logged in, and save their user object to a variable called @current_user
+  # helper_method line below current_user allows us to use @current_user in our view files
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+
+  # Authorize is for sending someone to the login page if they aren't logged in
+  def authorize
+    redirect_to '/login' unless current_user
+  end
+
   private
 
   def cart
