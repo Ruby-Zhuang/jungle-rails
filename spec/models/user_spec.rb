d7227aaa3b0eb882608ca7696aa268878eb8c258
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-31 15:04:43 -0400

    Add fuser model spec for a valid user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    before do
+      @user = User.new(:first_name => "Jane", :last_name => "Doe", :email => "test@example.com", :password => "secret", :password_confirmation => "secret")
+    end
+
+    it "should successfully saves a user given a first name, last name, email, password and password confirmation" do
+      @user.save
+      expect(@user).to be_valid
+      expect(@user.errors.full_messages).to be_empty
+    end
+  end
 end
