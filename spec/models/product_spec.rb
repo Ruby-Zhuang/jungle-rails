2efbd4154ec7bb8908b850dea6ea312eb7601cf7
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-31 14:42:06 -0400

    Add model specs for Product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,29 +5,45 @@ RSpec.describe Product, type: :model do
 
     before do
       @category = Category.new(:name => "Test Category")
+      @product = @category.products.new(:name => "Test Product", :price => 100, :quantity => 10)
     end
 
     it "successfully saves a product given a name, price, quantity, and category" do
-      @product = Product.new(:name => "Test Product", :price => 100, :quantity => 10, :category => @category)
-      @product.save!
+      @product.save
       expect(@product).to be_valid
       expect(@product.errors.full_messages).to be_empty
     end
 
     # validates :name, presence: true
     it "fails to save a product when no name is specified" do
+      @product.name = nil
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to_not be_empty
     end
 
     # validates :price, presence: true
     it "fails to save a product when no price is specified" do
+      @product.price_cents = nil
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to_not be_empty
     end
 
     # validates :quantity, presence: true
     it "fails to save a product when no quantity is specified" do
+      @product.quantity = nil
+      @product.save
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to_not be_empty
     end
 
     # validates :category, presence: true
     it "fails to save a product when no category is specified" do
+      @noCategoryProduct = Product.new(:name => "Test Product", :price => 100, :quantity => 10)
+      @noCategoryProduct.save
+      expect(@noCategoryProduct).to_not be_valid
+      expect(@noCategoryProduct.errors.full_messages).to_not be_empty
     end
 
   end
